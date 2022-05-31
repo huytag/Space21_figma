8f23b67976c2ae8a40e0588e1a7019d519ea3c46
huytag <huytagicloud@gmail.com>
2022-06-01 00:19:17 +0700

    add js tab account-information and panel

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,42 @@
 (function ($) {
   $(function () {
     $(document).ready(function () {
-      menuToggle();
-      clickbtnSwitch();
+      AccordionAccountUser();
     });
-    function menuToggle() {
-      const toggle = document.querySelector(".menu-toglle");
-      const logo = document.querySelector(".header-logo img");
-      const menu = document.querySelector(".header-menu");
-      const isShow = "is-show";
-      toggle.addEventListener("click", function () {
-        menu.classList.add(isShow);
-        logo.classList.add(isShow);
-        toggle.classList.add(isShow);
-      });
-      window.addEventListener("click", function (e) {
-        if (!menu.contains(e.target) && !e.target.matches(".menu-toglle")) {
-          menu.classList.remove(isShow);
-          logo.classList.remove(isShow);
-          toggle.classList.remove(isShow);
-        }
-      });
-    }
-    function clickbtnSwitch() {
-      const elbtn = $(".button-switch");
-      const elLeft = $(".text-left");
-      const elRight = $(".text-right");
-      let click = true;
-      if (elbtn.length > 0) {
-        elbtn.on("click", function () {
-          if (click) {
-            elbtn.removeClass("is-click");
-            elRight.addClass("text-white");
-            elLeft.removeClass("text-white");
-            click = false;
-          } else {
-            elbtn.addClass("is-click");
-
-            elLeft.addClass("text-white");
-            elRight.removeClass("text-white");
-            click = true;
-          }
-        });
+    // function menuToggle() {
+    //   const toggle = document.querySelector(".menu-toglle");
+    //   const logo = document.querySelector(".header-logo img");
+    //   const menu = document.querySelector(".header-menu");
+    //   const isShow = "is-show";
+    //   toggle.addEventListener("click", function () {
+    //     menu.classList.add(isShow);
+    //     logo.classList.add(isShow);
+    //     toggle.classList.add(isShow);
+    //   });
+    //   window.addEventListener("click", function (e) {
+    //     if (!menu.contains(e.target) && !e.target.matches(".menu-toglle")) {
+    //       menu.classList.remove(isShow);
+    //       logo.classList.remove(isShow);
+    //       toggle.classList.remove(isShow);
+    //     }
+    //   });
+    // }
+    function AccordionAccountUser() {
+      var acc = document.getElementsByClassName("accordion");
+      let view = $(".tab-accordion");
+      let i;
+      for (i = 0; i < acc.length; i++) {
+        (function (index) {
+          acc[i].addEventListener("click", function (e) {
+            this.classList.toggle("active");
+            var panel = view[index].nextElementSibling;
+            if (panel.style.maxHeight) {
+              panel.style.maxHeight = null;
+            } else {
+              panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+          });
+        })(i);
       }
     }
   });
